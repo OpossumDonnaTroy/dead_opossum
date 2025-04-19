2fbd9264b1805e8a6395f91a737c393b762bb4b8
OpossumDonnaTroy <motya050890@gmail.com>
2025-04-19 16:59:13 +0300

    переместила файлы старше 30 дней в архив

--- log_rotate/script_archivator.py
+++ log_rotate/script_archivator.py
@@ -1,23 +1,34 @@
 import os 
-import datetime
+from datetime import datetime, timedelta
+
 
 base_path = os.path.dirname(__file__)
 print(base_path)
 report_path = os.path.join(base_path, 'reports')
 #описала папку архив
 archive_path = os.path.join(base_path, 'archive')
-now = datetime.datetime.now()
+now = datetime.now()
 
 #получить список файлов из репортс
-fileslist = os.listdir(report_path,)
+fileslist = os.listdir(report_path)
 for filename in fileslist:
-    filename = filename[7:-4]
-    filedate = datetime.datetime.strptime(filename, '%Y-%m-%d')
-    print(filedate)
+    filedate = filename[7:-4]
+    filedate = datetime.strptime(filedate, '%Y-%m-%d')
+    today = datetime.now()
+    old_date = today - timedelta(days=30)
+    if filedate < old_date:
+        from_path = os.path.join(report_path, filename)
+        dest_path = os.path.join(archive_path, filename)
+        os.rename(from_path, dest_path)
+
+
+    
+    
     
 
 
 #выполнить отбор нужных файлов из папки репортс 
+
 #вывести теперь список без шелухи в названии
 
 
